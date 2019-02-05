cd779f941c5e549a3bd9d7c6791abc87a9bc5def
wKich <nflazzer@gmail.com>
2019-02-05 18:06:09 +0500

    add simple approval script

--- packages/react-ui-selenium/tests/Button.ts
+++ packages/react-ui-selenium/tests/Button.ts
@@ -3,7 +3,27 @@ import { By } from "selenium-webdriver";
 
 describe("Input", function() {
   describe("Inputs with different states", function() {
-    it.skip(["firefox", "ie11"], "should have expected text", async function() {
+    it("should have expected text 1", async function() {
+      const element = await this.browser.findElement(By.css("#test-element"));
+
+      return expect(await element.takeScreenshot()).to.matchImage("inputs");
+    });
+    it("should have expected text 2", async function() {
+      const element = await this.browser.findElement(By.css("#test-element"));
+
+      return expect(await element.takeScreenshot()).to.matchImage("inputs");
+    });
+    it("should have expected text 3", async function() {
+      const element = await this.browser.findElement(By.css("#test-element"));
+
+      return expect(await element.takeScreenshot()).to.matchImage("inputs");
+    });
+    it("should have expected text 4", async function() {
+      const element = await this.browser.findElement(By.css("#test-element"));
+
+      return expect(await element.takeScreenshot()).to.matchImage("inputs");
+    });
+    it("should have expected text 5", async function() {
       const element = await this.browser.findElement(By.css("#test-element"));
 
       return expect(await element.takeScreenshot()).to.matchImage("inputs");
